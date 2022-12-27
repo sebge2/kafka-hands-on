03e0a7002d57a08ee7a6cb6bfc32b1fed77181b7
Sébastien Gérard <me@sgerard.be>
2022-12-27 19:01:03 +0100

    consumer

--- wikimedia/consumer/src/main/java/be/sgerard/kafka/configuration/KafkaConfiguration.java
+++ wikimedia/consumer/src/main/java/be/sgerard/kafka/configuration/KafkaConfiguration.java
@@ -1,5 +1,7 @@
 package be.sgerard.kafka.configuration;
 
+import be.sgerard.kafka.model.dto.WikimediaEventDto;
+import com.fasterxml.jackson.databind.type.SimpleType;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +11,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,22 +27,25 @@ public class KafkaConfiguration {
     private String groupId;
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<String, WikimediaEventDto> consumerFactory() {
         final Map<String, Object> properties = new HashMap<>();
 
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         properties.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, "200");
         properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10");
 
-        return new DefaultKafkaConsumerFactory<>(properties);
+        var valueDeserializer = new JsonDeserializer<>(WikimediaEventDto.class);
+        valueDeserializer.typeResolver((topic, data, headers) -> SimpleType.constructUnsafe(WikimediaEventDto.class));
+
+        return new DefaultKafkaConsumerFactory<>(properties, new StringDeserializer(), valueDeserializer);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-        final ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String, WikimediaEventDto> kafkaListenerContainerFactory() {
+        final ConcurrentKafkaListenerContainerFactory<String, WikimediaEventDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
 
         // batch
