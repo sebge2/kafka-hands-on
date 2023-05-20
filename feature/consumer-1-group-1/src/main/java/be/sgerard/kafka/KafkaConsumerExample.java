116d782c0ab4b1c43a5be102a15db72afb6af2c3
Sébastien Gérard <me@sgerard.be>
2023-05-20 12:39:26 +0200

    listener

--- feature/consumer-1-group-1/src/main/java/be/sgerard/kafka/KafkaConsumerExample.java
+++ feature/consumer-1-group-1/src/main/java/be/sgerard/kafka/KafkaConsumerExample.java
@@ -1,13 +1,17 @@
 package be.sgerard.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.Properties;
+
+import static java.util.Collections.singleton;
 
 public class KafkaConsumerExample {
 
@@ -21,11 +25,23 @@ public class KafkaConsumerExample {
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
 
         try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties)) {
-            consumer.subscribe(Collections.singleton("test"));
+            consumer.subscribe(singleton("test"), new Listener());
 
             final ConsumerRecords<String, String> records = consumer.poll(Duration.ofMinutes(1));
 
             records.forEach(record -> System.out.println(record.value()));
+        }
+    }
+
+    private static class Listener implements ConsumerRebalanceListener {
+        @Override
+        public void onPartitionsRevoked(Collection<TopicPartition> collection) {
+            System.out.println("Partitions revoked " + collection);
+        }
+
+        @Override
+        public void onPartitionsAssigned(Collection<TopicPartition> collection) {
+            System.out.println("Partitions assigned " + collection);
         }
     }
 }
