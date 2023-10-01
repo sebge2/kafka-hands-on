fbf607d0657a284f89b8d8e5a594942c9232e5e0
Sébastien Gérard <me@sgerard.be>
2023-10-01 11:09:39 +0200

    stack

--- bin/kafka/stack/advanced-kraft/setup-ssl-certificates.sh
+++ bin/kafka/stack/advanced-kraft/setup-ssl-certificates.sh
@@ -5,27 +5,31 @@
 # https://www.baeldung.com/spring-boot-kafka-ssl
 # https://dalelane.co.uk/blog/?p=4399
 
+# shellcheck disable=SC2039
+CURRENT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 remove_ca_certificate() {
-  rm -f "./broker-ca.cert"
-  rm -f "./broker-ca.csr"
-  rm -f "./broker-ca.key"
-  rm -f "./client-ca.cert"
-  rm -f "./client-ca.csr"
-  rm -f "./client-ca.key"
-  rm -f "./.srl"
+  rm -f "$CURRENT_DIRECTORY/broker-ca.cert"
+  rm -f "$CURRENT_DIRECTORY/broker-ca.csr"
+  rm -f "$CURRENT_DIRECTORY/broker-ca.key"
+  rm -f "$CURRENT_DIRECTORY/client-ca.cert"
+  rm -f "$CURRENT_DIRECTORY/client-ca.csr"
+  rm -f "$CURRENT_DIRECTORY/client-ca.key"
+  rm -f "$CURRENT_DIRECTORY/.srl"
+  rm -f "$CURRENT_DIRECTORY/client-ca.srl"
 }
 
 generate_broker_ca_certificates() {
   echo "Generate Broker CA certificate"
-  openssl req -new -newkey rsa:2048 -nodes -keyout ./broker-ca.key -out ./broker-ca.csr -subj "/C=BE/CN=Broker CA"
-  openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=BE/CN=Broker CA" -keyout ./broker-ca.key  -out ./broker-ca.cert
+  openssl req -new -newkey rsa:2048 -nodes -keyout "$CURRENT_DIRECTORY/broker-ca.key" -out "$CURRENT_DIRECTORY/broker-ca.csr" -subj "/C=BE/CN=Broker CA"
+  openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=BE/CN=Broker CA" -keyout "$CURRENT_DIRECTORY/broker-ca.key"  -out "$CURRENT_DIRECTORY/broker-ca.cert"
   echo ""
 }
 
 generate_client_ca_certificates() {
   echo "Generate Client CA certificate"
-  openssl req -new -newkey rsa:2048 -nodes -keyout ./client-ca.key -out ./client-ca.csr -subj "/C=BE/CN=Client CA"
-  openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=BE/CN=Client CA" -keyout ./client-ca.key  -out ./client-ca.cert
+  openssl req -new -newkey rsa:2048 -nodes -keyout "$CURRENT_DIRECTORY/client-ca.key" -out "$CURRENT_DIRECTORY/client-ca.csr" -subj "/C=BE/CN=Client CA"
+  openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=BE/CN=Client CA" -keyout "$CURRENT_DIRECTORY/client-ca.key"  -out "$CURRENT_DIRECTORY/client-ca.cert"
   echo ""
 }
 
@@ -47,11 +51,11 @@ generate_client_certificate() {
   mkdir -p "$CLIENT_SSL_CONFIG_DIR"
 
   echo "Generate a truststore for client containing the client CA certificate"
-  keytool -keystore "$CLIENT_TRUSTSTORE_LOCATION" -storepass "$CLIENT_TRUSTSTORE_PASSWORD" -alias "ClientCARoot" -import -file "./client-ca.cert" -noprompt -storetype PKCS12
+  keytool -keystore "$CLIENT_TRUSTSTORE_LOCATION" -storepass "$CLIENT_TRUSTSTORE_PASSWORD" -alias "ClientCARoot" -import -file "$CURRENT_DIRECTORY/client-ca.cert" -noprompt -storetype PKCS12
   echo ""
 
   echo "Import broker CA certificate into client's truststore"
-  keytool -keystore "$CLIENT_TRUSTSTORE_LOCATION" -storepass "$CLIENT_TRUSTSTORE_PASSWORD" -alias "BrokerCARoot" -import -file "./broker-ca.cert" -noprompt
+  keytool -keystore "$CLIENT_TRUSTSTORE_LOCATION" -storepass "$CLIENT_TRUSTSTORE_PASSWORD" -alias "BrokerCARoot" -import -file "$CURRENT_DIRECTORY/broker-ca.cert" -noprompt
 
   echo "Generate a private key for client"
   keytool -keystore "$CLIENT_KEYSTORE_LOCATION" -storepass "$CLIENT_KEYSTORE_PASSWORD" -keypass "$CLIENT_KEYSTORE_PASSWORD" -alias "$CLIENT_NAME" -genkey -keyalg RSA -keysize 2048 -storetype PKCS12 -dname "CN=$CLIENT_NAME,O=SGerard,C=BE" -validity 365
@@ -62,15 +66,15 @@ generate_client_certificate() {
   echo ""
 
   echo "Sign client certificate with CA"
-  openssl x509 -req -CA "./client-ca.cert" -CAkey "./client-ca.key" -in "$CLIENT_CERTIFICATE_UNSIGNED" -out "$CLIENT_CERTIFICATE_SIGNED" -days 365 -CAcreateserial
+  openssl x509 -req -CA "$CURRENT_DIRECTORY/client-ca.cert" -CAkey "$CURRENT_DIRECTORY/client-ca.key" -in "$CLIENT_CERTIFICATE_UNSIGNED" -out "$CLIENT_CERTIFICATE_SIGNED" -days 365 -CAcreateserial
   echo ""
 
   echo "Import Broker CA certificate into client's keystore"
-  keytool -keystore "$CLIENT_KEYSTORE_LOCATION" -storepass "$CLIENT_KEYSTORE_PASSWORD" -keypass "$CLIENT_KEYSTORE_PASSWORD" -alias "BrokerCARoot" -import -file "./broker-ca.cert" -noprompt
+  keytool -keystore "$CLIENT_KEYSTORE_LOCATION" -storepass "$CLIENT_KEYSTORE_PASSWORD" -keypass "$CLIENT_KEYSTORE_PASSWORD" -alias "BrokerCARoot" -import -file "$CURRENT_DIRECTORY/broker-ca.cert" -noprompt
   echo ""
 
   echo "Import Client CA certificate into client's keystore"
-  keytool -keystore "$CLIENT_KEYSTORE_LOCATION" -storepass "$CLIENT_KEYSTORE_PASSWORD" -keypass "$CLIENT_KEYSTORE_PASSWORD" -alias "ClientCARoot" -import -file "./client-ca.cert" -noprompt
+  keytool -keystore "$CLIENT_KEYSTORE_LOCATION" -storepass "$CLIENT_KEYSTORE_PASSWORD" -keypass "$CLIENT_KEYSTORE_PASSWORD" -alias "ClientCARoot" -import -file "$CURRENT_DIRECTORY/client-ca.cert" -noprompt
   echo ""
 
   echo "Import client certificate into client's keystore"
@@ -88,7 +92,7 @@ ssl.key.password = $CLIENT_KEYSTORE_PASSWORD" > "$CLIENT_SSL_CONFIG_DIR/ssl-debu
 generate_broker_certificates() {
   BROKER_NAME=$1
   BROKE_HOSTNAME="kafka-$BROKER_NAME"
-  BROKER_SSL_CONFIG_DIR="../../volume/broker/$BROKER_NAME/ssl"
+  BROKER_SSL_CONFIG_DIR="$CURRENT_DIRECTORY/../../volume/broker/$BROKER_NAME/ssl"
 
   BROKER_TRUSTSTORE_LOCATION="$BROKER_SSL_CONFIG_DIR/server-$BROKER_NAME.ts.p12"
   BROKER_TRUSTSTORE_PASSWORD="server-$BROKER_NAME-ts-password"
@@ -136,15 +140,15 @@ remove_ca_certificate
 generate_broker_ca_certificates
 generate_client_ca_certificates
 
-generate_client_certificate "admin" "../../volume/broker/shared/ssl/admin" "/kafka/shared-ssl/admin"
-generate_client_certificate "sample-client-1" "../../volume/broker/shared/ssl/sample-client-1" "/kafka/shared-ssl/sample-client-1"
-generate_client_certificate "sample-client-2" "../../volume/broker/shared/ssl/sample-client-2" "/kafka/shared-ssl/sample-client-2"
+generate_client_certificate "admin" "$CURRENT_DIRECTORY/../../volume/broker/shared/ssl/admin" "/kafka/shared-ssl/admin"
+generate_client_certificate "sample-client-1" "$CURRENT_DIRECTORY/../../volume/broker/shared/ssl/sample-client-1" "/kafka/shared-ssl/sample-client-1"
+generate_client_certificate "sample-client-2" "$CURRENT_DIRECTORY/../../volume/broker/shared/ssl/sample-client-2" "/kafka/shared-ssl/sample-client-2"
 
-generate_client_certificate "ui-manager" "../../volume/ui-manager/ssl" "/ui-manager/ssl"
-generate_client_certificate "ksql-server" "../../volume/ksql-server/ssl" "/ksql/ssl"
-generate_client_certificate "schema-registry" "../../volume/schema-registry/ssl" "/schema-registry/ssl"
-generate_client_certificate "connect-1" "../../volume/connect/connect-1/ssl" "/connect/ssl"
-generate_client_certificate "connect-2" "../../volume/connect/connect-2/ssl" "/connect/ssl"
+generate_client_certificate "ui-manager" "$CURRENT_DIRECTORY/../../volume/ui-manager/ssl" "/ui-manager/ssl"
+generate_client_certificate "ksql-server" "$CURRENT_DIRECTORY/../../volume/ksql-server/ssl" "/ksql/ssl"
+generate_client_certificate "schema-registry" "$CURRENT_DIRECTORY/../../volume/schema-registry/ssl" "/schema-registry/ssl"
+generate_client_certificate "connect-1" "$CURRENT_DIRECTORY/../../volume/connect/connect-1/ssl" "/connect/ssl"
+generate_client_certificate "connect-2" "$CURRENT_DIRECTORY/../../volume/connect/connect-2/ssl" "/connect/ssl"
 
 generate_broker_certificates "broker-1"
 generate_broker_certificates "broker-2"
