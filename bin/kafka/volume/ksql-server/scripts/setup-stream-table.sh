fefb78a13d13196a39dbe7d99555b8b7b1b9a5df
Sébastien Gérard <me@sgerard.be>
2023-10-08 19:00:05 +0200

    ksql

--- bin/kafka/volume/ksql-server/scripts/setup-stream-table.sh
+++ bin/kafka/volume/ksql-server/scripts/setup-stream-table.sh
@@ -2,7 +2,7 @@
 
 execute_script() {
   FILE_NAME=$1
-  SCRIPT=$(cat "$FILE_NAME"| tr -d '\n' | tr -d '\r')
+  SCRIPT=$(cat "$FILE_NAME"| tr -d '\n' | tr -d '\r' | sed 's/"/\\"/g')
 
   RESULT=$(curl -s -X "POST" "http://localhost:8088/ksql" -H "Content-Type: application/vnd.ksql.v1+json; charset=utf-8"  -d '{"ksql":"'"$SCRIPT"'"}')
   RET=$?
@@ -13,17 +13,30 @@ execute_script() {
   elif [[ "$RESULT" =~ .*"A table with the same name already exists".* ]]
   then
     echo "Table already exists [$FILE_NAME]"
+  elif [[ "$RESULT" =~ .*"Connector".*"already exists".* ]]
+  then
+    echo "Connector already exists [$FILE_NAME]"
   elif [[ "$RESULT" =~ .*"Stream created".* ]]
   then
     echo "Stream created [$FILE_NAME]"
+  elif [[ "$RESULT" =~ .*"created and running".* ]]
+  then
+    echo "Stream created and running [$FILE_NAME]"
   elif [[ "$RESULT" =~ .*"Table created".* ]]
   then
     echo "Table created [$FILE_NAME]"
+  elif [[ "$RESULT" =~ .*"Insert Into query is running".* ]]
+  then
+    echo "Insert Into query running [$FILE_NAME]"
   else
     echo "Could not execute script [$FILE_NAME]"
     echo "Exit code: $RET"
+    echo ""
+    echo "Request: $SCRIPT"
+    echo ""
     echo "Response: $RESULT"
-    #exit 1
+    echo ""
+    exit 1
   fi
 }
 
